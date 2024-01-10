2bcc0cca28182c493ea1ce7c67d37b8f01a0ce81
Dinesh Jumani <jumanidineshdj@gmail.com>
2024-01-10 20:09:53 +0530

    Riverpod providers Navigator added

--- favorite_places/lib/main.dart
+++ favorite_places/lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:favorite_places/screens/places.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final colorScheme = ColorScheme.fromSeed(
@@ -27,7 +28,7 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-    const MyApp(),
+    const ProviderScope(child: MyApp()),
   );
 }
 
@@ -37,9 +38,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Great Places',
       theme: theme,
-      home: Container(),
+      home: const PlacesScreen(),
     );
   }
 }
