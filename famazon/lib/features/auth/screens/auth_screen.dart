66b48d43ec413d08acb5c5c962dc0365234b4494
Dinesh Jumani <jumanidineshdj@gmail.com>
2024-02-12 19:09:44 +0530

    Added new Project famazon

---
+++ famazon/lib/features/auth/screens/auth_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  static const String routeName = '/auth-screen';
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _AuthScreenState();
+  }
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ famazon/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:famazon/contsants/global_variables.dart';
+import 'package:famazon/features/auth/screens/auth_screen.dart';
+import 'package:famazon/router.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'famazon',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      onGenerateRoute: (settings) => gererateRoute(settings),
+      home: const AuthScreen(),
+    );
+  }
+}
