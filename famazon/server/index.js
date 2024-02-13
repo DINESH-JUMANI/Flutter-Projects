96a1d30f66367e56c2613ed9a08bda5910124cdc
Dinesh Jumani <jumanidineshdj@gmail.com>
2024-02-13 15:35:05 +0530

    Added auth logic plus comnnected mongoose db

--- famazon/server/index.js
+++ famazon/server/index.js
@@ -1,1 +1,28 @@
-console.log("Hello World");
+// IMPORTS FROM PACKAGES
+const express = require('express');
+const mongoose = require('mongoose');
+
+// IMPORTS FROM OTHER FILES
+const authRouter = require('./routes/auth');
+
+// INIT
+const PORT = 3000;
+const app = express();
+const DB = "mongodb+srv://dottdj:dini123@cluster0.y28gz2s.mongodb.net/?retryWrites=true&w=majority";
+
+// middleware
+app.use(authRouter);
+
+// Connections
+mongoose.connect(DB)
+    .then(() => {
+        console.log("Connection Successfull");
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+
+
+app.listen(PORT, () => {
+    console.log(`connected at port ${PORT} hello`)
+});
---
+++ famazon/server/routes/auth.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const User = require('../models/user');
+
+const authRouter = express.Router();
+
+authRouter.post('/api/signup', async (req, res) => {
+    // get data from client
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+
+
+
+
+    // post data in db
+    // return data to the user
+});
+
+module.exports = authRouter;
+
