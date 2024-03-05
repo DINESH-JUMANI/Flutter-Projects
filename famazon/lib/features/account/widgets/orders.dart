4bffb1de31da84c2f08e57059e235af7b0e7755f
Dinesh Jumani <jumanidieneshdj@gmail.com>
2024-03-05 12:36:24 +0530

    viewing products in my orders

--- famazon/lib/features/account/widgets/orders.dart
+++ famazon/lib/features/account/widgets/orders.dart
@@ -1,5 +1,8 @@
+import 'package:famazon/common/widgets/loader.dart';
 import 'package:famazon/contsants/global_variables.dart';
+import 'package:famazon/features/account/services/account_services.dart';
 import 'package:famazon/features/account/widgets/single_product.dart';
+import 'package:famazon/models/order.dart';
 import 'package:flutter/material.dart';
 
 class Orders extends StatefulWidget {
@@ -10,54 +13,62 @@ class Orders extends StatefulWidget {
 }
 
 class _OrdersState extends State<Orders> {
-  List list = [
-    'https://img.freepik.com/free-photo/smartphone-balancing-with-pink-background_23-2150271746.jpg?size=338&ext=jpg&ga=GA1.1.87170709.1707868800&semt=sph',
-    'https://img.freepik.com/free-photo/smartphone-balancing-with-pink-background_23-2150271746.jpg?size=338&ext=jpg&ga=GA1.1.87170709.1707868800&semt=sph',
-    'https://img.freepik.com/free-photo/smartphone-balancing-with-pink-background_23-2150271746.jpg?size=338&ext=jpg&ga=GA1.1.87170709.1707868800&semt=sph',
-    'https://img.freepik.com/free-photo/smartphone-balancing-with-pink-background_23-2150271746.jpg?size=338&ext=jpg&ga=GA1.1.87170709.1707868800&semt=sph',
-  ];
+  List<Order>? orders;
+  final AccountServices accountServices = AccountServices();
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void fetchOrders() async {
+    orders = await accountServices.fetchMyOrders(context: context);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              padding: const EdgeInsets.only(left: 15),
-              child: const Text(
-                'Your Orders',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
+    return orders == null
+        ? const Loader()
+        : Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(left: 15),
+                    child: const Text(
+                      'Your Orders',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(right: 15),
+                    child: Text(
+                      'See all',
+                      style: TextStyle(
+                        color: GlobalVariables.selectedNavBarColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                height: 170,
+                padding: const EdgeInsets.only(left: 10, top: 20, right: 0),
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: orders!.length,
+                  itemBuilder: (context, index) {
+                    return SingleProduct(
+                      image: orders![index].products[0].images[0],
+                    );
+                  },
                 ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(right: 15),
-              child: Text(
-                'See all',
-                style: TextStyle(
-                  color: GlobalVariables.selectedNavBarColor,
-                ),
-              ),
-            ),
-          ],
-        ),
-        Container(
-          height: 170,
-          padding: const EdgeInsets.only(left: 10, top: 20, right: 0),
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: list.length,
-            itemBuilder: (context, index) {
-              return SingleProduct(
-                image: list[index],
-              );
-            },
-          ),
-        ),
-      ],
-    );
+            ],
+          );
   }
 }
