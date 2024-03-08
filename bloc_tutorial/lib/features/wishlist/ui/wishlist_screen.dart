2531b30cdae8e1cdfbc7d709cf84bc764efd0a8b
Dinesh Jumani <jumanidieneshdj@gmail.com>
2024-03-08 19:35:47 +0530

    Added wishlist logic to add and remove wishlist products from wishlist screen

--- bloc_tutorial/lib/features/wishlist/ui/wishlist_screen.dart
+++ bloc_tutorial/lib/features/wishlist/ui/wishlist_screen.dart
@@ -1,4 +1,7 @@
+import 'package:bloc_tutorial/features/wishlist/bloc/wishlist_bloc.dart';
+import 'package:bloc_tutorial/features/wishlist/ui/wishlist_tile_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class WishlistScreen extends StatefulWidget {
   const WishlistScreen({super.key});
@@ -8,8 +11,52 @@ class WishlistScreen extends StatefulWidget {
 }
 
 class _WishlistScreenState extends State<WishlistScreen> {
+  final WishlistBloc wishlistBloc = WishlistBloc();
+  @override
+  void initState() {
+    wishlistBloc.add(WishlistInitialEvent());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.teal.shade300,
+        title: const Text('WishList'),
+      ),
+      body: BlocConsumer<WishlistBloc, WishlistState>(
+        bloc: wishlistBloc,
+        listenWhen: (previous, current) => current is WishlistActionState,
+        buildWhen: (previous, current) => current is! WishlistActionState,
+        listener: (context, state) {
+          if (state is WishlistRemovedFromWishlistActionState) {
+            ScaffoldMessenger.of(context).clearSnackBars();
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Item removed from wishlist'),
+              ),
+            );
+          }
+        },
+        builder: (context, state) {
+          switch (state.runtimeType) {
+            case WishlistSuccessState:
+              final successState = state as WishlistSuccessState;
+              return ListView.builder(
+                itemCount: successState.wishlistItems.length,
+                itemBuilder: (context, index) {
+                  return WishlistTileWidget(
+                      wishlistBloc: wishlistBloc,
+                      productDataModel: successState.wishlistItems[index]);
+                },
+              );
+            default:
+              return const SizedBox();
+          }
+        },
+      ),
+    );
   }
 }
