036e21ee0481a810a0016387cfed1a397b3cc352
Dinesh Jumani <jumanidieneshdj@gmail.com>
2024-03-08 14:28:08 +0530

    Added bloc files

---
+++ bloc_tutorial/lib/features/home/bloc/home_block_event.dart
@@ -0,0 +1,4 @@
+part of 'home_block_bloc.dart';
+
+@immutable
+sealed class HomeBlockEvent {}
---
+++ bloc_tutorial/lib/features/home/bloc/home_block_state.dart
@@ -0,0 +1,6 @@
+part of 'home_block_bloc.dart';
+
+@immutable
+sealed class HomeBlockState {}
+
+final class HomeBlockInitial extends HomeBlockState {}
