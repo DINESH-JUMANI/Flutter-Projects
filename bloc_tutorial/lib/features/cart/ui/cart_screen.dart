ef98a1a21092460f319d0544e6bd388c0c5a58d6
Dinesh Jumani <jumanidieneshdj@gmail.com>
2024-03-08 19:16:19 +0530

    Added cart remove logic in cart screen

--- bloc_tutorial/lib/features/cart/ui/cart_screen.dart
+++ bloc_tutorial/lib/features/cart/ui/cart_screen.dart
@@ -1,4 +1,7 @@
+import 'package:bloc_tutorial/features/cart/bloc/cart_bloc.dart';
+import 'package:bloc_tutorial/features/cart/ui/cart_tile_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -8,8 +11,52 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
+  final CartBloc cartBloc = CartBloc();
+  @override
+  void initState() {
+    cartBloc.add(CartInitialEvent());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.teal.shade300,
+        title: const Text('Cart'),
+      ),
+      body: BlocConsumer<CartBloc, CartState>(
+        bloc: cartBloc,
+        listenWhen: (previous, current) => current is CartActionState,
+        buildWhen: (previous, current) => current is! CartActionState,
+        listener: (context, state) {
+          if (state is CartRemovedFromCartActionState) {
+            ScaffoldMessenger.of(context).clearSnackBars();
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Item removed from cart'),
+              ),
+            );
+          }
+        },
+        builder: (context, state) {
+          switch (state.runtimeType) {
+            case CartSuccessState:
+              final successState = state as CartSuccessState;
+              return ListView.builder(
+                itemCount: successState.cartItems.length,
+                itemBuilder: (context, index) {
+                  return CartTileWidget(
+                      cartBloc: cartBloc,
+                      productDataModel: successState.cartItems[index]);
+                },
+              );
+            default:
+              return const SizedBox();
+          }
+        },
+      ),
+    );
   }
 }
